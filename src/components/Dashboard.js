d3521baa49b8a9bf59ecd9279b3b761ce19c4941
Dylan <dcren2010@gmail.com>
2021-05-20 12:26:38 -0400

    fetch data from server

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Loading from './Loading';
 import Panel from './Panel';
+import axios from 'axios'
 
 import classnames from 'classnames';
 
@@ -29,8 +30,11 @@ const data = [
 
 class Dashboard extends Component {
 	state = {
-		loading: false,
-		focused: null,
+		loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
 	};
 
 	componentDidMount() {
@@ -38,7 +42,23 @@ class Dashboard extends Component {
     if (focused) {
       // This is to keep the focused value stay whenever refresh the page
       this.setState({ focused });
-		}
+    }
+    
+    // Fetch data:
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')])
+      .then(
+			([days, appointments, interviewers]) => {
+				this.setState({
+					loading: false,
+					days: days.data,
+					appointments: appointments.data,
+					interviewers: interviewers.data,
+				});
+			}
+		);
 	}
 
   componentDidUpdate(previousProps, previousState) {
