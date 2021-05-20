ad07d22e431b6d4fd23dbf749939742fd6518bd1
Dylan <dcren2010@gmail.com>
2021-05-20 13:20:43 -0400

    adding lifcycle to classes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import Loading from './Loading';
 import Panel from './Panel';
 import axios from 'axios'
+import {
+ getTotalInterviews,
+ getLeastPopularTimeSlot,
+ getMostPopularDay,
+ getInterviewsPerDay
+} from "helpers/selectors";
+import { setInterview } from 'helpers/reducers';
 
 import classnames from 'classnames';
 
@@ -9,47 +16,43 @@ const data = [
 	{
 		id: 1,
 		label: 'Total Interviews',
-		value: 6,
+		getValue: getTotalInterviews,
 	},
 	{
 		id: 2,
 		label: 'Least Popular Time Slot',
-		value: '1pm',
+		getValue: getLeastPopularTimeSlot,
 	},
 	{
 		id: 3,
 		label: 'Most Popular Day',
-		value: 'Wednesday',
+		getValue: getMostPopularDay,
 	},
 	{
 		id: 4,
 		label: 'Interviews Per Day',
-		value: '2.3',
+		getValue: getInterviewsPerDay,
 	},
 ];
 
 class Dashboard extends Component {
 	state = {
 		loading: true,
-    focused: null,
-    days: [],
-    appointments: {},
-    interviewers: {}
+		focused: null,
+		days: [],
+		appointments: {},
+		interviewers: {},
 	};
 
 	componentDidMount() {
 		const focused = JSON.parse(localStorage.getItem('focused'));
-    if (focused) {
-      // This is to keep the focused value stay whenever refresh the page
-      this.setState({ focused });
-    }
-    
-    // Fetch data:
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')])
-      .then(
+		if (focused) {
+			// This is to keep the focused value stay whenever refresh the page
+			this.setState({ focused });
+		}
+
+		// Fetch data:
+		Promise.all([axios.get('/api/days'), axios.get('/api/appointments'), axios.get('/api/interviewers')]).then(
 			([days, appointments, interviewers]) => {
 				this.setState({
 					loading: false,
@@ -59,10 +62,21 @@ class Dashboard extends Component {
 				});
 			}
 		);
+
+		// Create a WebSockets instance variable
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    
+    this.socket.onmessage = (event) => {
+			const data = JSON.parse(event.data);
+
+			if (typeof data === 'object' && data.type === 'SET_INTERVIEW') {
+				this.setState((previousState) => setInterview(previousState, data.id, data.interview));
+			}
+		};
 	}
 
-  componentDidUpdate(previousProps, previousState) {
-    // This is to update the focused whenever the focused value changes, and save to localStorage
+	componentDidUpdate(previousProps, previousState) {
+		// This is to update the focused whenever the focused value changes, and save to localStorage
 		if (previousState.focused !== this.state.focused) {
 			localStorage.setItem('focused', JSON.stringify(this.state.focused));
 		}
@@ -70,8 +84,11 @@ class Dashboard extends Component {
 
 	selectPanel(id) {
 		// this.setState({focused: this.state.focused !== null ? null : id})
-    this.setState((prev) => ({ focused: prev.focused !== null ? null : id }));
-    console.log(this.state.focused);
+		this.setState((prev) => ({ focused: prev.focused !== null ? null : id }));
+	}
+
+	componentWillUnmount() {
+		this.socket.close();
 	}
 
 	render() {
@@ -88,7 +105,12 @@ class Dashboard extends Component {
 						{data
 							.filter((panel) => this.state.focused === null || this.state.focused === panel.id)
 							.map((panel) => (
-								<Panel key={panel.id} {...panel} onSelect={() => this.selectPanel(panel.id)} />
+								<Panel
+									key={panel.id}
+									label={panel.label}
+									value={panel.getValue(this.state)}
+									onSelect={() => this.selectPanel(panel.id)}
+								/>
 							))}
 					</main>
 				)}
