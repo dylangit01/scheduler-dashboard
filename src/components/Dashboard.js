5980ec2070c00e92c874bd16e29dfde9e41a0751
Dylan <dcren2010@gmail.com>
2021-05-20 08:42:44 -0400

    add state for displaying Loading page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
-
-import classnames from "classnames";
+import React, { Component } from 'react';
+import Loading from './Loading';
+import classnames from 'classnames';
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+	state = { loading: true };
+	render() {
+		const dashboardClasses = classnames('dashboard');
 
-    return <main className={dashboardClasses} />;
-  }
+		return <>{this.state.loading ? <Loading /> : <main className={dashboardClasses} />}</>;
+	}
 }
 
 export default Dashboard;
